410670ff8369ecbd4d7810862a0f80bed5e4533b
Shahrear Ahamed <its.shahrear@gmail.com>
2022-08-22 12:31:54 +0600

    filter section are add

--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -3,12 +3,40 @@ import Todo from "./Todo";
 
 export default function TodoList() {
   const todos = useSelector((state) => state.todos);
+  const filters = useSelector((state) => state.filters);
+  const { status, colors } = filters;
+
+  // filter seaction are here
+  const filterByStatus = (todo) => {
+    switch (status) {
+      case "Complete":
+        return todo.complated;
+
+      case "Incomplete":
+        return !todo.complated;
+
+      default:
+        return true;
+    }
+  };
+
+  const filterByColors = (todo) => {
+    if (colors.length > 0) {
+      return colors.includes(todo?.color);
+    }
+    return true;
+  };
   return (
     <div className="mt-2 text-gray-700 text-sm max-h-[300px] overflow-y-auto">
       {todos.length > 0 ? (
-        todos.map((todo) => <Todo key={todo.id} todo={todo} />)
+        todos
+          .filter(filterByStatus)
+          .filter(filterByColors)
+          .map((todo) => <Todo key={todo.id} todo={todo} />)
       ) : (
-        <h2 className="text-center font-semibold text-red-400 select-none">Nothing to show here</h2>
+        <h2 className="text-center font-semibold text-red-400 select-none">
+          Nothing to show here
+        </h2>
       )}
     </div>
   );
