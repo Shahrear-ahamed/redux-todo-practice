0216b9465c94c29771eac541af3da2096528bb36
Shahrear Ahamed <its.shahrear@gmail.com>
2022-08-22 11:28:59 +0600

    make complate all and clear completed

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,19 @@
+import { useDispatch } from "react-redux";
 import tickImage from "../assets/images/double-tick.png";
 import noteImage from "../assets/images/notes.png";
 import plusImage from "../assets/images/plus.png";
+import { allCompleted, clearCompleted } from "../redux/todos/actions";
 
 export default function Header() {
+  const dispatch = useDispatch();
+
+  const complatedAll = () => {
+    dispatch(allCompleted());
+  };
+
+  const handleClearComplated = () => {
+    dispatch(clearCompleted());
+  };
   return (
     <div>
       <form className="flex items-center bg-gray-100 px-4 py-4 rounded-md">
@@ -21,11 +32,13 @@ export default function Header() {
       </form>
 
       <ul className="flex justify-between my-4 text-xs text-gray-500">
-        <li className="flex space-x-1 cursor-pointer">
+        <li className="flex space-x-1 cursor-pointer" onClick={complatedAll}>
           <img className="w-4 h-4" src={tickImage} alt="Complete" />
           <span>Complete All Tasks</span>
         </li>
-        <li className="cursor-pointer">Clear completed</li>
+        <li className="cursor-pointer" onClick={handleClearComplated}>
+          Clear completed
+        </li>
       </ul>
     </div>
   );
